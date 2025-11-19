0a83208f4cb7cd50cb74e64b9bab76d11642c1e5
kdsavchenko <skld0@bk.ru>
2025-11-20 00:17:05 +0300

    no message

---
+++ EsiaUserBff/Service/Interface/IRequestStatusStore.cs
@@ -0,0 +1,7 @@
+namespace EsiaUserGenerator.Service.Interface;
+
+public interface IRequestStatusStore
+{
+    Task SetStatusAsync(string requestId, string status);
+    Task<string?> GetStatusAsync(string requestId);
+}
