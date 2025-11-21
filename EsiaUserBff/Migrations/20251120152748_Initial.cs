d07d40ad0c38c043f61b049dce3ad4a0611c0bd0
kosts0 <skld0@bk.ru>
2025-11-21 17:41:55 +0300

    added migrations

---
+++ EsiaUserBff/Migrations/20251120152748_Initial.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EsiaUserGenerator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EsiaUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Oid = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Login = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    CreatedRequestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EsiaUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CreatedHistory",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedRequestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreatedHistory", x => new { x.UserId, x.CreatedRequestId });
+                    table.ForeignKey(
+                        name: "FK_CreatedHistory_EsiaUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "EsiaUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RequestHistory",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    JsonRequest = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequestHistory", x => new { x.UserId, x.RequestId });
+                    table.ForeignKey(
+                        name: "FK_RequestHistory_EsiaUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "EsiaUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreatedHistory");
+
+            migrationBuilder.DropTable(
+                name: "RequestHistory");
+
+            migrationBuilder.DropTable(
+                name: "EsiaUsers");
+        }
+    }
+}
